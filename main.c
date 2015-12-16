fe9cecea6836584f58ca79336cf7baa6d5330907
Chunshang Li <jack.chunshang.li@gmail.com>
2015-12-15 20:28:39 -0800

    removed useless code

--- main.c
+++ main.c
@@ -48,82 +48,11 @@ void ConfigureUART(void)
     UARTStdioConfig(0, 115200, 16000000);
 }
 
-
-void test1(){
-	//    Mag Stuff
-	    i2c3_write_byte(0x1E, 0x02, 0x00);
-	    UARTprintf("Error: %5u\n", I2CMasterErr(I2C3_BASE));
-
-	    while(1){
-	    	uint8_t buff[6];
-	    	uint16_t x, y, z;
-	    	i2c3_read_bytes(0x1E, 0x03, buff, 6);
-	    	UARTprintf("Error: %5u\n", I2CMasterErr(I2C3_BASE));
-
-	//    	i2c3_read_byte(0x1E, 0x03, &buff[0]);
-	//    	i2c3_read_byte(0x1E, 0x04, &buff[1]);
-	//    	i2c3_read_byte(0x1E, 0x05, &buff[2]);
-	//    	i2c3_read_byte(0x1E, 0x06, &buff[3]);
-	//    	i2c3_read_byte(0x1E, 0x07, &buff[4]);
-	//    	i2c3_read_byte(0x1E, 0x08, &buff[5]);
-
-	    	x = (((uint16_t)buff[0]) << 8) | ((uint16_t)buff[1]);
-	    	y = (((uint16_t)buff[2]) << 8) | ((uint16_t)buff[3]);
-	    	z = (((uint16_t)buff[4]) << 8) | ((uint16_t)buff[5]);
-
-	    	UARTprintf("%5d %5d %5d\n", (int16_t)x, (int16_t)y, (int16_t)z);
-	    }
-}
-
-void test2(){
-
-//	#define    MPU9250_ADDRESS            0x68
-	#define    MAG_ADDRESS                0x0C
-
-	#define    GYRO_FULL_SCALE_250_DPS    0x00
-	#define    GYRO_FULL_SCALE_500_DPS    0x08
-	#define    GYRO_FULL_SCALE_1000_DPS   0x10
-	#define    GYRO_FULL_SCALE_2000_DPS   0x18
-
-	#define    ACC_FULL_SCALE_2_G        0x00
-	#define    ACC_FULL_SCALE_4_G        0x08
-	#define    ACC_FULL_SCALE_8_G        0x10
-	#define    ACC_FULL_SCALE_16_G       0x18
-
-    UARTprintf("Error: %5u\n", I2CMasterErr(I2C3_BASE));
-    i2c3_write_byte(0x68, 27, GYRO_FULL_SCALE_2000_DPS);
-    UARTprintf("Error: %5u\n", I2CMasterErr(I2C3_BASE));
-    i2c3_write_byte(0x68, 28, ACC_FULL_SCALE_4_G);
-    UARTprintf("Error: %5u\n", I2CMasterErr(I2C3_BASE));
-    i2c3_write_byte(0x68, 0x37, 0x02);
-    UARTprintf("Error: %5u\n", I2CMasterErr(I2C3_BASE));
-    i2c3_write_byte(MAG_ADDRESS, 0x0A, 0x01);
-    UARTprintf("Error: %5u\n", I2CMasterErr(I2C3_BASE));
-
-    while(1){
-    	uint8_t buff[14];
-    	int16_t ax, ay, az, gx, gy, gz;
-    	i2c3_read_bytes(0x68, 0x3B, buff, 14);
-    	UARTprintf("Error: %5u\n", I2CMasterErr(I2C3_BASE));
-
-    	ax = (int16_t)((((uint16_t)buff[0]) << 8) | ((uint16_t)buff[1]));
-    	ay = (int16_t)((((uint16_t)buff[2]) << 8) | ((uint16_t)buff[3]));
-    	az = (int16_t)((((uint16_t)buff[4]) << 8) | ((uint16_t)buff[5]));
-
-    	gx = (int16_t)((((uint16_t)buff[8]) << 8) | ((uint16_t)buff[9]));
-    	gy = (int16_t)((((uint16_t)buff[10]) << 8) | ((uint16_t)buff[11]));
-    	gz = (int16_t)((((uint16_t)buff[12]) << 8) | ((uint16_t)buff[13]));
-
-    	UARTprintf("Accel %5d %5d %5d\n", ax, ay, az);
-    	UARTprintf("Gyro  %5d %5d %5d\n", gx, gy, gz);
-    	delay_ms(250);
-    }
-}
-
-void test3(){
+void print_values_test(){
 	
 	int8_t ret;
 	
+	i2c3_init();
 	mpu9250_init();
 	
 	while(1){
@@ -153,12 +82,8 @@ int main(void) {
 
     ConfigureUART();
 
-    UARTprintf("MPU9250Test\n");
-
-
-    i2c3_init();
-    
-    test3();
+    UARTprintf("Program Start\n");
+    print_values_test();
 
 	return 0;
 }
